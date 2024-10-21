c914738ddbd6602d22a86b07cedf2a83b7ceaa3a
NakJoo <169232628+NJ0428@users.noreply.github.com>
2024-10-21 22:26:36 +0900

    Update SelectionsSort.java

--- src/algorithm/SelectionsSort.java
+++ src/algorithm/SelectionsSort.java
@@ -5,23 +5,19 @@ public class SelectionsSort {
 	public class SelectionSort {
 	    public static void main(String[] args) {
 	        int array[] = {2, 1, 10, 6, 5, 3, 7, 9, 4, 8};
-
 	        for (int i = 0; i < array.length - 1; i++) {
 	            int minIndex = i; // 현재 위치를 최소값의 인덱스로 초기화
-
 	            // 현재 위치 이후의 부분에서 최소값의 인덱스를 찾음
 	            for (int j = i + 1; j < array.length; j++) {
 	                if (array[j] < array[minIndex]) {
 	                    minIndex = j; // 최소값 인덱스 업데이트
 	                }
 	            }
-
 	            // 최소값을 현재 위치와 교환
 	            int temp = array[minIndex];
 	            array[minIndex] = array[i];
 	            array[i] = temp;
 	        }
-
 	        // 정렬된 배열 출력
 	        for (int i = 0; i < array.length; i++) {
 	            System.out.println(array[i]);
